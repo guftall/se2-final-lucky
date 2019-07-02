751ccb3a467bda270892f42f8d2e7e9adce4aa90
oMid <dehgkaniomid@gmail.com>
2019-07-02 18:15:44 +0430

    learn page for ch1

--- SE2(omid)/assets/script/luck_start_app_stub.js
+++ SE2(omid)/assets/script/luck_start_app_stub.js
@@ -3,10 +3,9 @@ Blockly.JavaScript['luck_start_app'] = function(block) {
   // var remainingCode = Blockly.JavaScript.valueToCode(block, 'INPUT_BB', Blockly.JavaScript.ORDER_ADDITION) || '0';
 
   console.log(statements_name);
+  // console.log(remainingCode);
 
-  document.s = statements_name;
-
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+  var code = statements_name;
+  console.log(code);
+  return code;;
 };
--- SE2(omid)/assets/script/lucky_addTowNumberStub.js
+++ SE2(omid)/assets/script/lucky_addTowNumberStub.js
@@ -2,7 +2,13 @@ Blockly.JavaScript['lucky_addtownumber'] = function(block) {
   var variable_firstvariable = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('firstVariable'), Blockly.Variables.NAME_TYPE);
   var variable_secendvariable = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('secendVariable'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+
+  document.omid1 = {};
+  document.omid2 = {};
+  document.omid1[variable_firstvariable] = NaN;
+  document.omid2[variable_firstvariable] = NaN;
+  console.log('SUCCESS');
+  var code = `${variable_firstvariable} + ${variable_secendvariable}`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
